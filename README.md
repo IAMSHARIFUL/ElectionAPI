0f22b4917b43d5880e31a292c7d0d2f05cbe7f71
IAMSHARIFUL <sharifulislam08031998@gmail.com>
2024-03-30 15:46:05 +0600

    Generated Spring Boot Source Files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ElectionAPI
+This is a sample backend project for Election system using SPring Boot.
---
+++ src/test/java/api/election/ElectionAPI/ElectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.election.ElectionAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
